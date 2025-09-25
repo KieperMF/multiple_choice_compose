dc2aacd4dec5d56b18bb21cec44e2ab55d84a570
kieperMF <kieperfmatheus@gmail.com>
2025-09-24 22:34:27 -0300

    update

--- app/src/main/java/com/example/multiplechoice/MainActivity.kt
+++ app/src/main/java/com/example/multiplechoice/MainActivity.kt
@@ -6,14 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -29,6 +33,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -53,6 +58,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier) {
     Column(modifier = modifier.fillMaxSize()) {
+        Header()
         LazyColumn {
             items(options) {
                 opt-> OptionContainer(opt)
@@ -85,8 +91,11 @@ fun OptionContainer(option: OptionClass) {
 
 @Composable
 fun Header(){
-    Box(modifier = Modifier){
-
+    Box(modifier = Modifier.background(Color.LightGray).padding(horizontal = 12.dp).height(80.dp).border(
+        border = BorderStroke(1.dp, Color.Gray),
+        shape = RoundedCornerShape(12.dp)).clip(RoundedCornerShape(12.dp))){
+        Text("Pick a fucking option son of a bitch!", textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(top = 24.dp).size(24.dp))
     }
 }
 
